b873e597fc8c304cdc8b4a902be0eb75915f2bc4
gyanchandra moirangthem <gyanchandra moirangthem>
2021-08-07 17:53:15 +0530

    simple form

---
+++ src/component/YoutubeForm.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function YoutubeForm() {
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" />
+        <label htmlFor="email">E-mail</label>
+        <input type="text" id="email" name="email" />
+        <label htmlFor="channel">Channel</label>
+        <input type="text" id="channel" name="channel" />
+
+      <button>submit</button>
+       </form>
+    </div>
+  );
+}
+
+export default YoutubeForm;
